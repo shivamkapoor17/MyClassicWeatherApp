d245d13a2596af977ad7585e8903aeb13807a529
unknown <kapoor17kapoor@gmail.com>
2019-02-13 16:57:35 +0530

    login and signup functionality done

--- weather.py
+++ weather.py
@@ -7,22 +7,24 @@
 #     print(r)
 #     json_data = r.json()
 #     pprint(json_data)
-from flask import Flask, flash, render_template, request, redirect, url_for
+from flask import Flask, flash, g, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_migrate import Migrate
 import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(25)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/weather'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class User(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     Email = db.Column(db.String(120), nullable=False)
     password = db.Column(db.String(80), nullable=False)
-    cities = db.relationship('City', backref='user', lazy=True)
+    cities = db.relationship('City', backref='user', lazy='dynamic')
 
 class City(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
@@ -35,15 +37,24 @@ class City(db.Model):
 
 
 # city temperature icon description sno datetime
-
-
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user' in session:
+        g.user = session['user']
 
 @app.route('/')
 def index():
+    if g.user:
+        return redirect(url_for('weather', username=g.user))
+
     return render_template('index.html')
 
 @app.route('/signup', methods=['GET','POST'])
 def signup():
+    if g.user:
+        return redirect(url_for('weather', username=g.user))
+
     if request.method == "POST":
         username = request.form.get('uname')
         email = request.form.get('email')
@@ -53,36 +64,51 @@ def signup():
         hashed_password = generate_password_hash(password, method='sha256')
 
         if password != repassword:
-            return render_template('signup.html')
             # flash('password not matched')
+            return render_template('signup.html')
 
-
+        #else
         user = User(username=username, Email=email, password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        return redirect(url_for('weather'))
+        session['user'] = username
+        return redirect(url_for('weather', username=username))
 
     return render_template('signup.html')
 
 @app.route('/login', methods=['GET','POST'])
 def login():
+    if g.user:
+        return redirect(url_for('weather', username=g.user))
 
     if request.method == 'POST':
         username = request.form.get('uname')
         password = request.form.get('password')
 
-        user = User.query.filter_by(username).first()
-        if user:
-            if check_password_hash(user.password, password):
-                #create session hare
-                return redirect(url_for('weather', name=user.username))
+        user = User.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.password, password):
+            session['user'] = user.username
+            return redirect(url_for('weather', username=user.username))
+
+        else:
+            #invalid credintials
+            pass
 
     return render_template('login.html')
 
-@app.route('/weather/<string:name>')
-@app.route('/weather')
-def weather():
+@app.route('/weather/<string:username>')
+def weather(username):
+    if g.user:
+        pass
     return render_template('weather.html')
+
+@app.route('/logout')
+def logout():
+    if g.user:
+        session.pop('user', None)
+
+    return redirect(url_for('index'))
 
 if __name__=="__main__":
     app.run(debug=True)
